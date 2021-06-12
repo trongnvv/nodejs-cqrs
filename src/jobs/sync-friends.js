4b49357a40e8f921b55f299708ee125b7d5bb9d0
trongnv <trongnv138@gmail.com>
2021-06-12 13:00:52 +0700

    sync create

--- src/jobs/sync-friends.js
+++ src/jobs/sync-friends.js
@@ -1,5 +1,17 @@
-const create = (data) => {
-  console.log(data);
+const { fetchAPI } = require("../utils");
+const { ELASTICSEARCH_HOST } = require("../../config");
+const create = async ({ index, data }) => {
+  try {
+    const { _id: refID, ...body } = data;
+    const rs = await fetchAPI({
+      url: `${ELASTICSEARCH_HOST}/${index}/_doc`,
+      method: "POST",
+      data: { refID, ...body },
+    });
+    console.log(rs);
+  } catch (error) {
+    console.log("[KAFKA][ERROR]", error.response.data);
+  }
 };
 
 const edit = (data) => {};
